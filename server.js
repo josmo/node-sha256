9c57681f51ab675e93c813b33840399a824db501
Joachim Hill-Grannec <joachim.hill@gmail.com>
2018-07-03 19:42:20 -0400

    quick update to find x in string binary variations.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import config from 'config'
 import cache from 'memory-cache';
 import sha from 'sha256'
+import { vsprintf } from 'sprintf-js';
 const router = express.Router();
 const app = express();
 const appPort = config.get('service.port');
@@ -36,4 +37,37 @@ router.get('/messages/:id', (req,res) => {
 router.get('/healthcheck', (req, res) => {
  res.send('yep yep the service is up');
 });
+
+
+
+router.get('/xs/:id', (req, res) => {
+  const value = `${req.params.id}`;
+
+  const count = value.split('x').length - 1;
+  const templateString = value.replace(/x/g, "%s");
+
+  const result = [];
+
+  //I don't like mutating arrays like this. But this is just the quick solution
+  for (let variation = 0; variation < Math.pow(2, count); variation++) {
+    const combo = [];
+    for (let pos = 0; pos < count; pos++) {
+      //shift bit and and it with 1
+      if ((variation >> pos) & 1)
+        combo.push('0');
+      else
+        combo.push('1');
+    }
+    result.push(vsprintf(templateString, combo));
+  }
+  res.json(result);
+});
+
+router.post('/items/', (req,res) => {
+  cache.put('items', req.body);
+  res.json({message:'updated'})
+});
+
+
+
 
